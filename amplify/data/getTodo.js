acec9b555d1a56112bd1974d25e0712f88cee516
Robert Noneman <robert.noneman@ngc.com>
2025-03-11 18:01:39 -0400

    remove the things

--- amplify/data/getTodo.js
+++
@@ -1,7 +0,0 @@
-import * as ddb from "@aws-appsync/utils/dynamodb";
-
-export function request(ctx) {
-  return ddb.get({ key: { id: ctx.args.id } });
-}
-
-export const response = (ctx) => ctx.result;
