ec75dc75b3654d1b9ce0960ef1e1be951ceb4a8a
Robert Noneman <robert.noneman@ngc.com>
2025-03-13 15:43:20 -0400

    screw the sad confetti

--- app/components/RobdayConfetti..tsx
+++ app/components/RobdayConfetti..tsx
@@ -1,32 +1,33 @@
 'use client';
 
 import Pride from "react-canvas-confetti/dist/presets/pride";
+import Explosion from "react-canvas-confetti/dist/presets/explosion";
 import confetti from "canvas-confetti"
 import randomInRange from "react-canvas-confetti/dist/helpers/randomInRange";
 
 export default function RobdayConfetti() {
   const isRobday = new Date().getDay() === 1;
-  const sadFace = confetti.shapeFromText("😢");
-  const decorateOptions = {
-      // particleCount: 3,
-      angle: 60,
-      spread: 55,
-      origin: { x: 0 },
-      colors: ["#FF69B4", "#00FF00", "#FFA500"],
-      scalar: randomInRange(0.5, 15.5),
-      particleCount: randomInRange(1, 2),
-      shapes: [sadFace]
-    };
-    const decorateOptions2 = {
-      // particleCount: 3,
-      angle: 120,
-      spread: 55,
-      origin: { x: 1 },
-      colors: ["#FF69B4", "#00FF00", "#FFA500"],
-      scalar: randomInRange(0.5, 15.5),
-      particleCount: randomInRange(1, 2),
-      shapes: [sadFace]
-    };
+  // const sadFace = confetti.shapeFromText("😢");
+  // const decorateOptions = {
+  //     // particleCount: 3,
+  //     angle: 60,
+  //     spread: 55,
+  //     origin: { x: 0 },
+  //     colors: ["#FF69B4", "#00FF00", "#FFA500"],
+  //     scalar: randomInRange(0.5, 15.5),
+  //     particleCount: randomInRange(1, 2),
+  //     shapes: [sadFace]
+  //   };
+  //   const decorateOptions2 = {
+  //     // particleCount: 3,
+  //     angle: 120,
+  //     spread: 55,
+  //     origin: { x: 1 },
+  //     colors: ["#FF69B4", "#00FF00", "#FFA500"],
+  //     scalar: randomInRange(0.5, 15.5),
+  //     particleCount: randomInRange(1, 2),
+  //     shapes: [sadFace]
+  //   };
     
   return (
     <div>
@@ -34,8 +35,9 @@ export default function RobdayConfetti() {
           <Pride autorun={{ speed: 60, duration: 5000 }} />
         ) : (
           <div>
-          <Pride autorun={{ speed: 60, duration: 5000 }} decorateOptions={() => decorateOptions} />
-          <Pride autorun={{ speed: 60, duration: 5000 }} decorateOptions={() => decorateOptions2} />
+            <Explosion autorun={{ speed: 60, duration: 500 }} />
+          {/* <Pride autorun={{ speed: 60, duration: 5000 }} decorateOptions={() => decorateOptions} /> */}
+          {/* <Pride autorun={{ speed: 60, duration: 5000 }} decorateOptions={() => decorateOptions2} /> */}
           </div>
         )}
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import Image from "next/image";
 import { AuthGetCurrentUserServer, cookiesClient } from "@/utils/amplify-utils";
 import { revalidatePath } from "next/cache";
